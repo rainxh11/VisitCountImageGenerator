836ffa243f1e2837915436fc703aea6406176a21
Ahmed Chakhoum <a.chakhoum@netcom-group.co>
2024-09-27 22:26:25 +0100

    + added docker

--- Extensions.cs
+++ Extensions.cs
@@ -8,59 +8,58 @@ namespace VisitCountImageGenerator;
 
 public static class Extensions
 {
-    public static IImageProcessingContext ConvertToAvatar(this IImageProcessingContext processingContext,
-                                                          Size size,
-                                                          float cornerRadius)
-    {
-        return processingContext.Resize(new ResizeOptions
-                                        {
-                                            Size = size,
-                                            Mode = ResizeMode.Crop
-                                        }).ApplyRoundedCorners(cornerRadius);
-    }
+  public static IImageProcessingContext ConvertToAvatar(this IImageProcessingContext processingContext,
+                                                        Size size,
+                                                        float cornerRadius)
+  {
+    return processingContext.Resize(new ResizeOptions
+                                    {
+                                      Size = size,
+                                      Mode = ResizeMode.Crop
+                                    }).ApplyRoundedCorners(cornerRadius);
+  }
 
-    public static IImageProcessingContext ApplyRoundedCorners(this IImageProcessingContext ctx,
-                                                              float cornerRadius)
-    {
-        var size    = ctx.GetCurrentSize();
-        var corners = BuildCorners(size.Width, size.Height, cornerRadius);
+  private static IImageProcessingContext ApplyRoundedCorners(this IImageProcessingContext ctx,
+                                                             float cornerRadius)
+  {
+    var size = ctx.GetCurrentSize();
+    var corners = BuildCorners(size.Width, size.Height, cornerRadius);
 
-        ctx.SetGraphicsOptions(new GraphicsOptions()
-                               {
-                                   Antialias = true,
-                                   AlphaCompositionMode =
-                                       PixelAlphaCompositionMode
-                                          .DestOut // enforces that any part of this shape that has color is punched out of the background
-                               });
+    ctx.SetGraphicsOptions(new GraphicsOptions
+                           {
+                             Antialias = true,
+                             AlphaCompositionMode =
+                               PixelAlphaCompositionMode
+                                .DestOut // enforces that any part of this shape that has color is punched out of the background
+                           });
 
-        // mutating in here as we already have a cloned original
-        // use any color (not Transparent), so the corners will be clipped
-        foreach (var c in corners) ctx = ctx.Fill(Color.Red, c);
+    // mutating in here as we already have a cloned original
+    // use any color (not Transparent), so the corners will be clipped
 
-        return ctx;
-    }
+    return corners.Aggregate(ctx, (current, c) => current.Fill(Color.Red, c));
+  }
 
-    public static IPathCollection BuildCorners(int imageWidth,
-                                               int imageHeight,
-                                               float cornerRadius)
-    {
-        // first create a square
-        var rect = new RectangularPolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
+  private static IPathCollection BuildCorners(int imageWidth,
+                                              int imageHeight,
+                                              float cornerRadius)
+  {
+    // first create a square
+    var rect = new RectangularPolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
 
-        // then cut out of the square a circle so we are left with a corner
-        var cornerTopLeft = rect.Clip(new EllipsePolygon(cornerRadius - 0.5f, cornerRadius - 0.5f, cornerRadius));
+    // then cut out of the square a circle so we are left with a corner
+    var cornerTopLeft = rect.Clip(new EllipsePolygon(cornerRadius - 0.5f, cornerRadius - 0.5f, cornerRadius));
 
-        // corner is now a corner shape positions top left
-        //lets make 3 more positioned correctly, we can do that by translating the original around the center of the image
+    // corner is now a corner shape positions top left
+    //lets make 3 more positioned correctly, we can do that by translating the original around the center of the image
 
-        var rightPos  = imageWidth - cornerTopLeft.Bounds.Width + 1;
-        var bottomPos = imageHeight - cornerTopLeft.Bounds.Height + 1;
+    var rightPos = imageWidth - cornerTopLeft.Bounds.Width + 1;
+    var bottomPos = imageHeight - cornerTopLeft.Bounds.Height + 1;
 
-        // move it across the width of the image - the width of the shape
-        var cornerTopRight    = cornerTopLeft.RotateDegree(90).Translate(rightPos, 0);
-        var cornerBottomLeft  = cornerTopLeft.RotateDegree(-90).Translate(0, bottomPos);
-        var cornerBottomRight = cornerTopLeft.RotateDegree(180).Translate(rightPos, bottomPos);
+    // move it across the width of the image - the width of the shape
+    var cornerTopRight = cornerTopLeft.RotateDegree(90).Translate(rightPos, 0);
+    var cornerBottomLeft = cornerTopLeft.RotateDegree(-90).Translate(0, bottomPos);
+    var cornerBottomRight = cornerTopLeft.RotateDegree(180).Translate(rightPos, bottomPos);
 
-        return new PathCollection(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight);
-    }
+    return new PathCollection(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight);
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,183 +1,180 @@
 using System.Globalization;
-
 using LiteDB;
 using LiteDB.Async;
-
 using Microsoft.AspNetCore.Mvc;
-
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
-
+using SixLabors.ImageSharp.Processing;
 using VisitCountImageGenerator;
-
-using static System.Net.Mime.MediaTypeNames;
-
-using BsonDocument = LiteDB.BsonDocument;
 using Image = SixLabors.ImageSharp.Image;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Logging
+
+var logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Verbose)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(theme: AnsiConsoleTheme.Code, restrictedToMinimumLevel: LogEventLevel.Information);
+
+Log.Logger = logger.CreateLogger();
+builder.Host.UseSerilog();
+builder.Services.AddSerilog();
+
+#endregion
+
 builder.Services
        .AddLogging()
        .ConfigureHttpJsonOptions(options => options.SerializerOptions.WriteIndented = true)
-       .AddSingleton<ILiteDatabaseAsync>(
-                                         new
-                                             LiteDatabaseAsync($"Filename={Path.Combine(AppContext.BaseDirectory, "visits.db")};Connection=direct"))
+       .AddSingleton<ILiteDatabaseAsync>(new LiteDatabaseAsync(builder.Configuration["ConnectionString"]))
        .AddSingleton<Font>(provider =>
-        {
-            FontCollection collection = new();
-            collection.AddSystemFonts(metrics =>
-                                          metrics.Description.FontName(CultureInfo.CurrentCulture) == "Consolas")
-                      .Get("Arial");
-            var family = collection.Add(Path.Combine(AppContext.BaseDirectory, "Caskaydia.otf"));
-            var font   = family.CreateFont(48, FontStyle.Regular);
-            return font;
-        })
+                           {
+                             FontCollection collection = new();
+                             var family = collection.Add(Path.Combine(AppContext.BaseDirectory, "Caskaydia.otf"));
+                             var font = family.CreateFont(48, FontStyle.Regular);
+                             return font;
+                           })
        .AddSingleton<CounterImage>();
 
 var app = builder.Build();
 
 app.Use(async (ctx,
                next) =>
-{
-    try
-    {
-        ctx.Response.Headers.Append("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate");
-        await next(ctx);
-    }
-    catch (Exception ex)
-    {
-        ctx.Response.StatusCode = 500;
-        await ctx.Response.CompleteAsync();
-    }
-});
+        {
+          try
+          {
+            ctx.Response.Headers.Append("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate");
+            await next(ctx);
+          }
+          catch (Exception ex)
+          {
+            ctx.Response.StatusCode = 500;
+            await ctx.Response.CompleteAsync();
+          }
+        });
 
 app.MapGet("/visit-count/query", async (
-               [FromServices] ILiteDatabaseAsync db) =>
-           {
-               var visits = await db.GetCollection<Visit>()
-                                    .FindAllAsync();
+                                   [FromServices] ILiteDatabaseAsync db) =>
+                                 {
+                                   var visits = await db.GetCollection<Visit>()
+                                                        .FindAllAsync();
 
-               return new
-                      {
-                          Stats = visits.GroupBy(x => x.Username,
-                                                 x =>
-                                                     ((string[])x.Request["Headers"])
-                                                    .FirstOrDefault(x => x.Contains("X-Forwarded-For"),
-                                                                    null))
-                                        .ToDictionary(x => x.Key,
-                                                      x => new
-                                                           {
-                                                               Count    = visits.Count(x => x is not null),
-                                                               Visitors = x.Where(x => x is not null)
-                                                           }),
-                          Grouped = visits.GroupBy(x => x.Username).ToDictionary(x => x.Key, x => x)
-                      };
-           });
+                                   return new
+                                          {
+                                            Stats = visits.GroupBy(x => x.Username,
+                                                                   x => ((string[])x.Request["Headers"])
+                                                                    .FirstOrDefault(s => s?.Contains("X-Forwarded-For") is true, null))
+                                                          .ToDictionary(x => x.Key,
+                                                                        x => new
+                                                                             {
+                                                                               Count = visits.Count(visit => visit is not null),
+                                                                               Visitors = x.Where(s => s is not null)
+                                                                             }),
+                                            Grouped = visits.GroupBy(x => x.Username).ToDictionary(x => x.Key, x => x)
+                                          };
+                                 });
 
 app.MapGet("/visit-count/query/{username}", async (
-               string username,
-               [FromServices] ILiteDatabaseAsync db) =>
-           {
-               var visits = await db.GetCollection<Visit>()
-                                    .Query()
-                                    .Where(x => x.Username == username)
-                                    .ToListAsync();
+                                              string username,
+                                              [FromServices] ILiteDatabaseAsync db) =>
+                                            {
+                                              var visits = await db.GetCollection<Visit>()
+                                                                   .Query()
+                                                                   .Where(x => x.Username == username)
+                                                                   .ToListAsync();
 
-               return visits.OrderByDescending(x => x.CreatedOn);
-           });
+                                              return visits.OrderByDescending(x => x.CreatedOn);
+                                            });
 
 app.MapGet("/visit-count/generate/{username}.png", async (
-               string username,
-               [FromServices] ILiteDatabaseAsync db,
-               [FromServices] CounterImage counter,
-               [FromServices] ILogger<Visit> logger,
-               HttpRequest request) =>
-           {
-               var requestDict = new Dictionary<string, object>()
-                                 {
-                                     { "Cookies", request.Cookies.ToList() },
-                                     { "Headers", request.Headers.Select(x => x.ToString()).ToList() },
-                                     { "Host", request.Host },
-                                     { "Query", request.Query.Keys.ToList() },
-                                     { "Path", request.Path }
-                                 };
+                                                     string username,
+                                                     [FromServices] ILiteDatabaseAsync db,
+                                                     [FromServices] CounterImage counter,
+                                                     [FromServices] ILogger<Visit> logger,
+                                                     HttpRequest request) =>
+                                                   {
+                                                     var requestDict = new Dictionary<string, object>
+                                                                       {
+                                                                         { "Cookies", request.Cookies.ToList() },
+                                                                         { "Headers", request.Headers.Select(x => x.ToString()).ToList() },
+                                                                         { "Host", request.Host },
+                                                                         { "Query", request.Query.Keys.ToList() },
+                                                                         { "Path", request.Path }
+                                                                       };
 
 
-               var visit = new Visit() { Request = requestDict, Username = username };
-               await db.GetCollection<Visit>()
-                       .InsertAsync(visit);
+                                                     var visit = new Visit { Request = requestDict, Username = username };
+                                                     await db.GetCollection<Visit>()
+                                                             .InsertAsync(visit);
 
-               var count = await db.GetCollection<Visit>()
-                                   .Query()
-                                   .Where(x => x.Username == username)
-                                   .LongCountAsync();
+                                                     var count = await db.GetCollection<Visit>()
+                                                                         .Query()
+                                                                         .Where(x => x.Username == username)
+                                                                         .LongCountAsync();
 
-               var image = await counter.GenerateImage($"{count:N0}");
+                                                     var image = await counter.GenerateImage($"{count:N0}");
 
-               logger?.LogInformation($"'{username}' | {count:N0} visits");
+                                                     logger?.LogInformation("For: {@userName} | View Count: {@viewCount:N0}", username, count);
 
-               return Results.File(image, "image/png");
-           });
+                                                     return Results.File(image, "image/png");
+                                                   });
 
-app.Run("http://*:9000");
+app.Run();
 
 internal class Visit
 {
-    public Visit()
-    {
-    }
-
-    public string Username { get; set; }
-    public Dictionary<string, object> Request { get; set; }
-    [BsonId(true)] public int Id { get; set; }
-    public DateTime CreatedOn { get; set; } = DateTime.Now;
+  public string Username { get; set; }
+  public Dictionary<string, object> Request { get; set; }
+  [BsonId(true)] public int Id { get; set; }
+  public DateTime CreatedOn { get; set; } = DateTime.Now;
 }
 
 internal class CounterImage
 {
-    private Font _font;
+  private readonly Font _font;
 
-    public CounterImage(Font font)
-    {
-        _font = font;
-    }
+  public CounterImage(Font font)
+  {
+    _font = font;
+  }
 
-    public async Task<byte[]> GenerateImage(string text)
-    {
-        var imageBytes = await File.ReadAllBytesAsync(Path.Combine(AppContext.BaseDirectory, "VisitCount.png"));
-        var _image     = Image.Load(imageBytes);
-        var y          = 10;
-        var x          = 1080;
+  public async Task<byte[]> GenerateImage(string text)
+  {
+    var imageBytes = await File.ReadAllBytesAsync(Path.Combine(AppContext.BaseDirectory, "VisitCount.png"));
+    using var image = Image.Load(imageBytes);
+    const int y = 10;
+    const int x = 1080;
 
-        TextOptions options = new(_font)
-                              {
-                                  Origin              = new PointF(x, y),
-                                  HorizontalAlignment = HorizontalAlignment.Right
-                              };
+    var options = new RichTextOptions(_font)
+                  {
+                    Origin = new PointF(x, y),
+                    HorizontalAlignment = HorizontalAlignment.Right
+                  };
 
-        TextOptions grayOptions = new(_font)
-                                  {
-                                      Origin              = new PointF(825, y),
-                                      HorizontalAlignment = HorizontalAlignment.Left
-                                  };
+    var grayOptions = new RichTextOptions(_font)
+                      {
+                        Origin = new PointF(825, y),
+                        HorizontalAlignment = HorizontalAlignment.Left
+                      };
 
-        var brush     = Brushes.Solid(Color.Cyan);
-        var grayBrush = Brushes.Solid(Color.Gray);
-        var pen       = Pens.DashDot(Color.Cyan, 5);
+    var brush = Brushes.Solid(Color.Cyan);
+    var grayBrush = Brushes.Solid(Color.Gray);
+    var pen = Pens.DashDot(Color.Cyan, 5);
 
-        var grayText = "0".PadLeft(9 - text.Length, '0');
-        _image.Mutate(x => x.DrawText(options, text, brush)
-                            .DrawText(grayOptions, grayText, grayBrush)
-                     );
+    var grayText = "0".PadLeft(9 - text.Length, '0');
+    image.Mutate(imageProcessingContext => imageProcessingContext.DrawText(options, text, brush)
+                                                                 .DrawText(grayOptions, grayText, grayBrush)
+                );
 
-        var result = _image.Clone(x => x.ConvertToAvatar(_image.Size(), 20));
+    var result = image.Clone(context => context.ConvertToAvatar(image.Size, 20));
 
-        var stream = new MemoryStream();
-        await result.SaveAsPngAsync(stream);
-        return stream.ToArray();
-    }
+    var stream = new MemoryStream();
+    await result.SaveAsPngAsync(stream);
+    return stream.ToArray();
+  }
 }
